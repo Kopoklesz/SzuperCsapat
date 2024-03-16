ed322a9a39c1e37740b12b94e3af2bbf20d8088f
Kuti Bence <136807498+Kopoklesz@users.noreply.github.com>
2024-03-16 17:37:33 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let Lbutton = document.querySelector(".Lbtn")
+
+Lbutton.onclick = function(){
+        let username = document.getElementById("username").value
+        let password = document.getElementById("password").value
+}
