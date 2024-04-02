55821059545aebbd51140106de38406a817f7c1e
Kuti Bence <136807498+Kopoklesz@users.noreply.github.com>
2024-04-02 13:50:37 +0200

    Add files via upload

--- c/script.js
+++ c/script.js
@@ -1,52 +1,95 @@
 function login() {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "lib.php", true); // POST kérés
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var response = xhr.responseText;
-        }
-        if (response === "success") {
-                console.log("SUCCESS-LOG")
-        } else {
-                console.log("FAIL-LOG")
-        }
-    };
-    xhr.send("action=login&username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password)); // POST adatok küldése
-}
-
-
-function reg() {
-        let name = document.getElementById("name").value;
-        let username = document.getElementById("username").value;
-        let password = document.getElementById("password").value;
-
+        var username = document.getElementById("loginUsername").value;
+        var password = document.getElementById("loginPassword").value;
+    
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", "lib.php", true); // POST kérés
+        xhr.open("POST", "lib.php", true); // POST request
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4 && xhr.status === 200) {
+            if (xhr.readyState === 4 && xhr.status === 200) {
                 var response = xhr.responseText;
-                }
                 if (response === "success") {
-                        console.log("SUCCESS-REG")
+                    window.location.href = "mainpage.html";
                 } else {
-                        console.log("FAIL-REG")
+                    showToast();
+                    document.getElementById("loginUsername").value = "";
+                    document.getElementById("loginPassword").value = "";
                 }
+            }
         };
-        xhr.send("action=reg&name=" + encodeURIComponent(name) + "&username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password)); // POST adatok küldése
-}
-
-
-function loginPage(){
+        xhr.send("action=login&username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password)); // Sending POST data
+    }
+    
+    function reg() {
+        let name = document.getElementById("regName").value;
+        let username = document.getElementById("regUsername").value;
+        let password = document.getElementById("regPassword").value;
+    
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "lib.php", true); // POST request
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var response = xhr.responseText;
+                if (response === "success") {
+                    loginPage();
+                } else {
+                    showToast()
+                }
+            }
+        };
+        xhr.send("action=reg&name=" + encodeURIComponent(name) + "&username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password)); // Sending POST data
+    }
+    
+    function loginPage(){
         document.getElementById("loginPage").classList.remove("hidden");
         document.getElementById("regPage").classList.add("hidden"); 
-}
-
-function regPage(){
+    }
+    
+    function regPage(){
         document.getElementById("loginPage").classList.add("hidden");
         document.getElementById("regPage").classList.remove("hidden");
-}
+    }
+    
+    function showToast() {
+        var toast = document.getElementById("toast");
+        toast.textContent = "Hiba történt! Kérjük, ellenőrizze az adatokat.";
+        toast.classList.remove("hide");
+        setTimeout(function() {
+            toast.classList.add("hide");
+        }, 3000);
+    }
+
+    function categories(page){
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "lib.php", true); // POST request
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        if(page == "mainpage" || page == "create_post"){
+            xhr.send("action=categoryName");
+        } else {
+            xhr.send();
+        }
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                try {
+                    var response = JSON.parse(xhr.responseText);
+                    if (page == "mainpage") {
+                        makeCategories(response);
+                    }else if(page == "create_post"){
+                        makeCategories(response);
+                    }
+                } catch (e) {
+                    console.error("Parsing error:", e);
+                }
+            }
+        };
+    }
+
+    function makeCategories(response){
+        var category = document.getElementById("postCategory");
+        for (var i = 0; i < response.length; i++) {
+            var option = document.createElement("option");
+            option.text = response[i].name;
+            category.add(option);
+        }
+    }
